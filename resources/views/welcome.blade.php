d34e271ee3ed67fed9a9a6b61d6605e70427cb36
RampsosPeck <peralta.jorge.uatf@gmail.com>
2019-09-03 15:07:03 -0400

    Finish perfect class 10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,10 +92,30 @@
         </div>
     </body>
 </html>-->
+@extends('layouts.app')
 
-<form action="{{route('statuses.store')}}" method="POST">
-    @csrf
-<textarea name="body" id="" cols="30" rows="10"></textarea>
-<button id="create-status" >Publicar Estado</button>
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-8 mx-auto">
+            <div class="card border-0 bg-light mb-3 shadow-sm">
 
-</form>
+                    <div class="card-body">
+                           <textarea
+                              class="form-control border-0 bg-light"
+                              name="body"
+                              placeholder="¿Qué estás pensando Jorge peralta?"></textarea>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-primary" id="create-status">Publicar</button>
+                    </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+
+
